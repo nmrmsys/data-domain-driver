e17375ba42fb7d96db41498ca5783525dad751b6
Masayoshi Nomura <nmrmsys@gmail.com>
2019-03-28 18:05:32 +0900

    Add ResultSet feature

--- test2.js
+++ test2.js
@@ -7,6 +7,13 @@ FLD1,FLD2,FLD3
 a,b,c
 d,e,f
 `);
-
 console.log(rs);
 
+// console.log(rs.getRow(3));
+// rs.setRow(1, {FLD1: 'a1', FLD3: 'c3' });
+// rs.addRow({FLD1: 'a1', FLD2: 'b2', FLD3: 'c3' });
+// console.log(rs.getValue(2,2));
+// rs.setValue(1,2,'x');
+// console.log(rs.rows);
+// console.log(rs.clone());
+// console.log(rs.copy());
