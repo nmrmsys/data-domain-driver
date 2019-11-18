93eaab3844ea482d26e234daa0647b654fd08ba9
Masayoshi Nomura <nmrmsys@gmail.com>
2019-11-18 11:01:11 +0900

    Add LoggerWrapper level prop, dynamic level change

--- test3.js
+++ test3.js
@@ -1,11 +1,10 @@
 
 loggerWrapper = require('./lib/LoggerWrapper');
 log = loggerWrapper({level:'debug'});
-
-// console.log(logger);
+// log.level = 'error';
 log('aaa');
 log.info('bbb %s', 'prm');
 log.error('ccc');
 log.verbose('ddd');
 log.debug('eee');
-
+console.log(log.level);
