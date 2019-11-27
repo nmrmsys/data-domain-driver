bdb4b59b81cbe8f899cf8399cb8cc4443239c513
Masayoshi Nomura <nmrmsys@gmail.com>
2019-11-27 13:27:56 +0900

    Add app-root-path, Change include path relative to absolute

--- ddd/func/func01.js
+++ ddd/func/func01.js
@@ -6,6 +6,7 @@ module.exports = class func01 extends Function {
     async process(ddd, params, returns) {
 
         log('func01 process');
+        var rs = await ddd.query('select * from tbl1');
 
         return 0; // returns.returnCode
     }
