c3e7349811c0f4310a6685f7f0ebf9f73894bca2
Masayoshi Nomura <nmrmsys@gmail.com>
2019-12-03 18:06:06 +0900

    Add DAM toJSON relation feature

--- test4.js
+++ test4.js
@@ -8,43 +8,59 @@ ds = new DataSet();
 // ds = new DataSet();
 // _.assign(ds, rtns);
 
-RESULT_SET_1 = new ResultSet(`
-FLD1,FLD2,FLD3
-a,b,c
-d,e,f
+ORDERS = new ResultSet(`
+ORDER_ID,NAME
+1,aaa
+2,bbb
+3,ccc
 `);
 
-rs2 = new ResultSet(`
-FLD1,FLD2
-1,2
-3,4
+DETAILS = new ResultSet(`
+DETAIL_ID,ORDER_ID,ITEM,QTY
+1,1,xxx,1
+2,1,yyy,2
+3,1,zzz,3
+4,2,xxx,4
+5,2,yyy,5
+6,3,zzz,6
 `);
 
-ds.RESULT_SET_1 = RESULT_SET_1;
-ds.rs2 = rs2;
+ds.ORDERS = ORDERS;
+ds.DETAILS = DETAILS;
 ds.returnCode = 0;
 ds.returnCode2 = 0;
 
 //console.log('%O', ds);
-// console.log(RESULT_SET_1.dump());
+// console.log(ORDERS.dump());
 // console.log(ds.dump());
 // Utils = require('./lib/Utils');
 // console.log(Utils.dump(ds));
 
 // console.log(ds.toJSON());
-// console.log(ds.toJSON('RESULT_SET_1'));
-// console.log(ds.toJSON('rst=RESULT_SET_1[key]'));
+// console.log(ds.toJSON('ORDERS'));
+// console.log(ds.toJSON('rst=ORDERS[key]'));
 // console.log('%O', ds.toObject());
-// console.log('ddd/model/Order'.match(/.*\/(.*)/)[0]);
-// console.log('%O', Utils.toObject('../ddd/model/Order',{FLD1:1,FLD2:2}));
-// console.log('%O', Utils.toObject('../ddd/model/Order',rs2));
-// console.log('%O', Utils.toObject('../ddd/model/Order', [{FLD1:'1',FLD2:'2'},{FLD1:'3',FLD2:'4'}]));
-// console.log('%O', Utils.toObject('ddd/model/Order',{FLD1:1,FLD2:2}));
-// console.log('%O', Utils.toObject('ddd/model/Order', [{FLD1:'1',FLD2:'2'},{FLD1:'3',FLD2:'4'}]));
-const config = require('config');
-const config_ddd = config.util.toObject(config.get('ddd'));
-// const DDD = new (require('data-domain-driver').DDD)(config_ddd);
-const DDD = new (require('./index').DDD)(config_ddd);
-Utils.setDDD(DDD);
-// console.log('%O', Utils.toObject('Order',{FLD1:1,FLD2:2}));
-console.log('%O', Utils.toObject('Order',rs2));
+// console.log('ddd/model/ORDERS'.match(/.*\/(.*)/)[0]);
+// console.log('%O', Utils.toObject({FLD1:1,FLD2:2},'../ddd/model/ORDERS'));
+// console.log('%O', Utils.toObject(DETAILS,'../ddd/model/ORDERS'));
+// console.log('%O', Utils.toObject([{FLD1:'1',FLD2:'2'},{FLD1:'3',FLD2:'4'}],'../ddd/model/ORDERS'));
+// console.log('%O', Utils.toObject({FLD1:1,FLD2:2},'ddd/model/ORDERS'));
+// console.log('%O', Utils.toObject([{FLD1:'1',FLD2:'2'},{FLD1:'3',FLD2:'4'}],'ddd/model/ORDERS'));
+// console.log('%O', Utils.toObject(ds));
+// console.log('%O', Utils.toObject(ds,'ddd/model/ORDERS=DETAILS'));
+// console.log('%O', Utils.toObject(ds,['ddd/model/ORDERS','DETAILS','']));
+
+console.log(ds.toJSON('orders=ORDERS[ORDER_ID]',{details:'DETAILS[ORDER_ID]'}));
+// console.log('%O', Utils.toObject(ORDERS,'ddd/model/Order'));
+// console.log('%O', Utils._getFilter('', {fld1:1, fld2:2, fld3:3}, ''));
+
+// const config = require('config');
+// const config_ddd = config.util.toObject(config.get('ddd'));
+// // const DDD = new (require('data-domain-driver').DDD)(config_ddd);
+// const DDD = new (require('./index').DDD)(config_ddd);
+// Utils.setDDD(DDD);
+// // console.log('%O', Utils.toObject({FLD1:1,FLD2:2},'ORDERS'));
+// console.log('%O', Utils.toObject(DETAILS,'ORDERS'));
+// ds._DDD = DDD;
+// console.log('%O', ds.toObject('ORDERS=DETAILS'));
+
